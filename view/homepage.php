f34f8ce59cba33f63c297476780dffe09d605fff
root <root@localhost.localdomain>
2012-08-16 14:31:10 +0530

    Added README.md

---
+++ view/homepage.php
@@ -0,0 +1,81 @@
+<html>
+
+	<head>
+		<title> home </title>
+		<link rel="stylesheet" type="text/css" href="home_page.css">
+	</head>
+	
+	<body>
+	<div id="main">
+		<div id="inner1">
+			<a href="" style="text-decoration: none; color: black; float: left;"> <h1> SchoolTime </h1></a><br />
+			<a href="" style="text-decoration: none; color: black; float: right;"> <h3> Dashboard </h3></a>
+			<a href="" style="text-decoration: none; color: black; float: right;"> <h3> Profile </h3></a>
+			<a href="" style="text-decoration: none; color: black; float: right;"> <h3> Home </h3></a>
+		</div>
+		<br />
+		<div id="inner2">
+			<div id="welcome">
+				Welcome, --username--
+			</div>
+		
+			<div id="form1">
+				<form name="form1">
+					Find People <input type="text" name="txtfindpeople"> <input type="submit" style="background: url(../images/search.jpg) no-repeat;" value="">			
+				</form>
+			</div>
+			<br /><br /> <br />
+			<div id="GC">
+				<div style="background-color: #348781;"> 
+					Groups Created
+				</div>
+				<br />
+				<div style="overflow: scroll; height: 74%;">
+					No groups created yet
+				</div> 
+			</div>
+			
+			<div id="GJ">
+				<div style="background-color: #348781;"> 
+					Groups Joined
+				</div>
+				<br />
+				<div style="overflow: scroll; height: 74%;">
+					No groups joined yet
+				</div> 
+			</div>
+			
+			<div id="OG">
+				<div style="background-color: #348781;"> 
+					Open Group Suggessions.
+				</div>
+				<br />
+				<div style="overflow: scroll; height: 74%;">
+					No group suggessions.
+				</div> 
+			</div>
+		
+			
+			<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+			<div id="image_menu">
+				<img src="../images/male-symbol.jpg" height="80" width="80"> <br /><br />
+				<a href="#" style="text-decoration: none; color: black;">Message Box </a><br />
+				<a href="#" style="text-decoration: none; color: black;">Activities </a>
+			</div>			
+			
+			<div id="updates">
+			
+			</div>			
+			
+			<div id="connections">
+				Connections &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; count; <br /> <br />
+				Recomended &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; count;  <br /> <br />
+				Recomending &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; count; <br />
+			</div>			
+			
+		</div>
+	</div>
+	</body>
+	
+</html>
